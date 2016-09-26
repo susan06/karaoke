322b632ea72488317b8145d165cd29f45906aa92
susan <susangmedina@gmail.com>
2016-09-26 19:55:03 -0400

    correcion

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -108,7 +108,7 @@ class SocialAuthController extends Controller
             $this->users->updateSocialNetworks($user->id, []);
 
             $role = $this->roles->findByName('user');
-            $user->attachRole($$role);
+            $user->attachRole($role);
         }
 
         // Associate social account with user account inside our application
