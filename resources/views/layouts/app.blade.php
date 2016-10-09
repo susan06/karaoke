90735e1e595825a6bf7563531a540abe8abc69d8
Susan Medina <susangmedina@gmail.com>
2016-10-09 18:56:46 -0300

    autor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
+    <!-- @autor susangmedina@gmail.com -->
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
