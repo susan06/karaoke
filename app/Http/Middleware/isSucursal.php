c221c3accb3b422bc4c83574d86feafa3162b762
Susan Medina <susangmedina@gmail.com>
2017-07-13 14:43:22 -0400

    auth is sucursal

--- app/Http/Middleware/isSucursal.php
+++ app/Http/Middleware/isSucursal.php
@@ -4,9 +4,28 @@ namespace App\Http\Middleware;
 
 use Auth;
 use Closure;
+use Illuminate\Contracts\Auth\Guard;
 
 class isSucursal
 {
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard  $auth
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
     /**
      * Handle an incoming request.
      *
@@ -16,14 +35,19 @@ class isSucursal
      */
     public function handle($request, Closure $next)
     {
-        if(session('branch_office') == null) {
-            if(Auth::user()->hasRole('user')) {
-              return Redirect()->route('song.search');
-            } else {
-              return redirect()->route('dashboard');
+        if ($this->auth->check()) {
+            if(session('branch_office') == null) {
+                if(Auth::user()->hasRole('user')) {
+                  return Redirect()->route('song.search');
+                } else {
+                  return redirect()->route('dashboard');
+                }
             }
+        } else {
+
+            return redirect()->guest('login');
         }
 
-        return $next($request);
+        //return $next($request);
     }
 }
